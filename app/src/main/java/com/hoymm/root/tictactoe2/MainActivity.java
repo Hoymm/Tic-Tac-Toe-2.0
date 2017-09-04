5f2a2d3d7857d04833e2c4a5e0f67e950e308de3
Hoymm <hoymm241@wp.pl>
2017-09-04 07:58:21 +0200

    New project of TicTacToe 2.0

---
+++ app/src/main/java/com/hoymm/root/tictactoe2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hoymm.root.tictactoe2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
