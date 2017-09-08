6b89b18fa0d00094d186c37a2e0efdc49b0dc351
Hoymm <hoymm241@wp.pl>
2017-09-08 18:04:35 +0200

    Add header fragment to main activity

--- app/src/main/java/com/hoymm/root/tictactoe2/MainActivity.java
+++ app/src/main/java/com/hoymm/root/tictactoe2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.hoymm.root.tictactoe2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inflateHeaderView();
+    }
+
+    private void inflateHeaderView() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        HeaderFragment headerFragment = new HeaderFragment();
+        fragmentTransaction.add(R.id.mainActivityHeader, headerFragment);
+        fragmentTransaction.commit();
     }
 }
