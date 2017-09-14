fd24ece36b68efb896d745ac383d0965eedb46e3
Hoymm <hoymm241@wp.pl>
2017-09-14 12:19:50 +0200

    Remove top bar and set application on fullscreen

--- app/src/main/java/com/hoymm/root/tictactoe2/MainActivity.java
+++ app/src/main/java/com/hoymm/root/tictactoe2/MainActivity.java
@@ -1,7 +1,6 @@
 package com.hoymm.root.tictactoe2;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
