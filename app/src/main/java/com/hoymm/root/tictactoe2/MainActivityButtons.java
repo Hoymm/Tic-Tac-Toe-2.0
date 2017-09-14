d600fa45c785600160d15f357783fbfafd8ad8c8
Hoymm <hoymm241@wp.pl>
2017-09-14 09:00:39 +0200

    Make "Single Button" open SingleButton.java activity

--- app/src/main/java/com/hoymm/root/tictactoe2/MainActivityButtons.java
+++ app/src/main/java/com/hoymm/root/tictactoe2/MainActivityButtons.java
@@ -1,10 +1,15 @@
 package com.hoymm.root.tictactoe2;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.hoymm.root.tictactoe2.SinglePlayer.SinglePlayer;
 
 /**
  * Created by Damian Muca - Kaizen (12.09.17)
@@ -22,11 +27,32 @@ public class MainActivityButtons extends ButtonsFragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         setButtonsTextNames();
+        setButtonsOnClickAction();
     }
 
     private void setButtonsTextNames() {
         String [] buttonTextNames = getContext().getResources().getStringArray(R.array.mainActivityButtonNames);
         for (int i = 0; i < buttons.length; ++i)
             buttons[i].setText(buttonTextNames[i]);
+    }
+
+    private void setButtonsOnClickAction() {
+        buttons[0].setOnClickListener(getSinglePlayerButtonAction());
+    }
+
+    @NonNull
+    private View.OnClickListener getSinglePlayerButtonAction() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startNewActivityAndFinishCurrent(new SinglePlayer());
+            }
+        };
+    }
+
+    private void startNewActivityAndFinishCurrent(Activity newActivity){
+        Intent intent = new Intent(getActivity(), newActivity.getClass());
+        getActivity().startActivity(intent);
+        getActivity().finish();
     }
 }
