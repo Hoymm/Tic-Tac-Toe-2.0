5e232f5e3932dcafb556bc1baaf0dc47202ec3ad
Damian <hoymm241@wp.pl>
2017-11-10 11:27:59 +0100

    Adjust margins of game board to fit screen correctly

--- app/src/main/java/com/hoymm/root/tictactoe2/GameEngine/GameBoardFragment.java
+++ app/src/main/java/com/hoymm/root/tictactoe2/GameEngine/GameBoardFragment.java
@@ -1,8 +1,13 @@
 package com.hoymm.root.tictactoe2.GameEngine;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +15,7 @@ import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.hoymm.root.tictactoe2.R;
 
 import static com.hoymm.root.tictactoe2.GameEngine.GameEngine.GAME_BOARD_SIZE_KEY;
@@ -19,6 +25,7 @@ import static com.hoymm.root.tictactoe2.GameEngine.GameEngine.GAME_BOARD_SIZE_KE
  */
 
 public class GameBoardFragment extends Fragment {
+    private static final int fieldMargin = 1;
     private BoardSize boardSize;
 
     @Nullable
@@ -48,7 +55,8 @@ public class GameBoardFragment extends Fragment {
 
         int boardLength = Math.min(generalLinearLayout.getWidth(), generalLinearLayout.getHeight());
         int howManyFieldsInRow = getHowManyFieldsInRow();
-        int fieldLength = boardLength/howManyFieldsInRow;
+
+        int fieldLength = (boardLength/howManyFieldsInRow);
 
         for (int row = 0; row < howManyFieldsInRow; ++row)
             insertBoardRow(generalLinearLayout, howManyFieldsInRow, fieldLength);
@@ -66,8 +74,13 @@ public class GameBoardFragment extends Fragment {
         }
     }
 
+    private static int dpToPx(int dp)
+    {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
+
     private void insertBoardRow(LinearLayout generalLinearLayout, int howManyFieldsInRow, int fieldLength) {
-        LinearLayout rowLinearLayout = createRowLinearLayout(fieldLength);
+        LinearLayout rowLinearLayout = createRowLinearLayout(fieldLength+fieldMargin*2);
         generalLinearLayout.addView(rowLinearLayout);
         addGameFieldsToFillRow(howManyFieldsInRow, fieldLength, rowLinearLayout);
     }
@@ -78,7 +91,9 @@ public class GameBoardFragment extends Fragment {
                 (ViewGroup.LayoutParams.MATCH_PARENT, height);
         //Log.i("HeightToSet", height + ".");
         rowLinearLayout.setLayoutParams(rowParams);
+        rowLinearLayout.setGravity(Gravity.CENTER);
         rowLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
+        rowLinearLayout.setBackgroundColor(Color.RED);
         return rowLinearLayout;
     }
 
@@ -87,7 +102,7 @@ public class GameBoardFragment extends Fragment {
             rowLinearLayout.addView(createNewBoardField(fieldLength));
     }
 
-    private Button createNewBoardField(int sideLength) {
+    private LottieAnimationView createNewBoardField(int fieldLength) {
 
         /*LottieAnimationView animationView = view.findViewById(R.id.animation_id);
         animationView.setRotation(45);
@@ -95,10 +110,22 @@ public class GameBoardFragment extends Fragment {
         animationView.loop(true);
         animationView.playAnimation();*/
 
-        Button myButton = new Button(getContext());
-        LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, sideLength, 1);
-        myButton.setLayoutParams(buttonParams);
-        myButton.setText("COS TAM");
-        return myButton;
+        LottieAnimationView myLottieAnimationView = new LottieAnimationView(getContext());
+        LinearLayout.LayoutParams fieldParams = new LinearLayout.LayoutParams(fieldLength, fieldLength);
+        fieldParams.setMargins(fieldMargin, fieldMargin, fieldMargin, fieldMargin);
+
+        myLottieAnimationView.setLayoutParams(fieldParams);
+        myLottieAnimationView.setBackgroundColor(Color.BLUE);
+
+
+        myLottieAnimationView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((LottieAnimationView)v).setAnimation("circle.json");
+                ((LottieAnimationView)v).playAnimation();
+            }
+        });
+
+        return myLottieAnimationView;
     }
 }
